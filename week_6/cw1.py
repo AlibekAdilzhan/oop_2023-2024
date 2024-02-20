16c48131d7c97a66a89cee85a9b8aae64650498c
student <student@almau.edu.kz>
2024-02-20 10:35:28 +0600

    week 6

---
+++ week_6/cw1.py
@@ -0,0 +1,24 @@
+class Dog:
+    def talk(self):
+        print("gaf")
+
+
+class Cat:
+    def talk(self):
+        print("meow")
+
+
+class Horse:
+    def talk(self):
+        print("igogo")
+
+
+class Fox:
+    def say(self):
+        print("what does the fox say")
+
+
+d = Dog()
+c = Cat()
+h = Horse()
+f = Fox()
---
+++ week_6/cw2.py
@@ -0,0 +1,23 @@
+class Human:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def info(self):
+        return f"{self.name} {self.age} {self.gender}"
+    
+
+
+class Doctor(Human):
+    def __init__(self, name, age, gender, speciality):
+        super().__init__(name, age, gender)
+        self.spec = speciality
+
+    def info(self):
+        return f"{self.name} {self.age} {self.gender} {self.spec}"
+
+
+d = Doctor("docok", 33, "m", "Surgery")
+
+print(d.info())
